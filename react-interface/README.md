12074b658246d31c5120c53ba9d76bbebb55276b
monikau-vgs <36798308+ukirdemonika@users.noreply.github.com>
2026-02-13 12:08:28 +0100

    1.set up and install, react icons, extensions

---
+++ react-interface/README.md
@@ -0,0 +1,5 @@
+Install extensions:
+    1.React development tool
+    2. Js/JSX snippnet
+    3. react snippet
+    4. npm install react-icons --save
---
+++ react-interface/src/App.js
@@ -0,0 +1,11 @@
+import { BiArchive } from "react-icons/bi";
+function App() {
+  return (
+    <div className="App">
+      <h1>Building interfaces with React</h1>
+      <BiArchive size={30} color="blue" />
+    </div>
+  );
+}
+
+export default App;
