3246f034c8fe477fdb2ce26cb2e7233161ab8c13
monikau-vgs <36798308+ukirdemonika@users.noreply.github.com>
2026-02-17 11:56:45 +0100

    creating sub component hook

--- react-interface/src/components/Search.jsx
+++ react-interface/src/components/Search.jsx
@@ -1,27 +1,57 @@
-import { BiSearch } from "react-icons/bi";
-import { BiCaretDown } from "react-icons/bi";
-const Search= () =>{
-    return(
-        <div className="py-5">
-            <div className="mt-1 relative rounded-md shadow-sm">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <BiSearch />
-                
-                <label  htmlFor="query" className="sr-only" />
+import { BiSearch,BiCheck ,BiCaretDown} from "react-icons/bi";
 
-            </div>
-            <input type="text" name= "query" id = "query" value=""
-            className=" pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
-           <div className="absolute inset-y-0 right-0 flex items-center">
+const DropDown = () => {
+  return (
+    <div className="Origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+      <div  className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu" >
+        <div  className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer flex justify-between "
+          role="menuitem">Pet Name<BiCheck /></div>
+        <div className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer flex justify-between "
+          role="menuitem">Owner Name<BiCheck /></div>
+        <div className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer flex justify-between "
+          role="menuitem">Date<BiCheck /></div>
+        <div className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer flex justify-between "
+          role="menuitem">Asc<BiCheck /></div>
+        <div className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer flex justify-between "
+          role="menuitem">Dsc<BiCheck /></div>
+      </div>
+    </div>
+  );
+};
+
+const Search = () => {
+  return (
+    <div className="py-5">
+      <div className="mt-1 relative rounded-md shadow-sm">
+        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <BiSearch />
+        </div>
+        <label htmlFor="query" className="sr-only" />
+        <input
+          type="text"
+          name="query"
+          id="query"
+          value=""
+          className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300"
+          placeholder="Search"
+        />
+        <div className="absolute inset-y-0 right-0 flex items-center">
           <div>
-            <button type="button"
-              className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
+            <button
+              type="button"
+              className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center"
+              id="options-menu"
+              aria-haspopup="true"
+              aria-expanded="true"
+            >
               Sort By <BiCaretDown className="ml-2" />
             </button>
+            <DropDown />
           </div>
         </div>
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+
 export default Search;
