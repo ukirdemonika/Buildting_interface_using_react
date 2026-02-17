fecf44453ab750a04db49ca289432e3e57cd0da2
monikau-vgs <36798308+ukirdemonika@users.noreply.github.com>
2026-02-17 11:30:08 +0100

    Search component-

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -1,11 +1,13 @@
 import { BiArchive } from "react-icons/bi";
+import Search from "./components/Search";
+
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">Building interfaces with React</h1>
-      <span className="inline-block text-red-400">
-        <BiArchive size={30} className="align-top" />
-      </span>
+      <h1 className="text-5xl ">Building interfaces with React</h1>
+      
+        <BiArchive size={30} className="align-top inline-block text-red-400"  />
+      <Search/>
     </div>
   );
 }
